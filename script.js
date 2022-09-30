6939d01991b66246f6bf587d27a05294ac7a010c
Mohansharma13 <msharma13613@gmail.com>
2022-09-30 18:32:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$("#carouselExampleCaptions").slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    mobileFirst: true
+});
